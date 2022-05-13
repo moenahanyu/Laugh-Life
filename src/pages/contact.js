bb4a3e8e85d58d32f41b160b804fe5e25374f40c
moenahanyu <0205.aile@gmail.com>
2022-05-13 12:39:55 +0900

    contact change

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from '../components/seo';
-import ContactForm from '../components/ContactForm'; 
+import ContactForm from '../components/contactForm'; 
 
 export default () => (
   <Layout>
