b9783e2efe4a7e8fce9f2d6d0ed0c2ec0bc6e8ec
moenahanyu <0205.aile@gmail.com>
2022-05-16 19:09:08 +0900

    humberger btn

--- src/components/header.js
+++ src/components/header.js
@@ -2,27 +2,61 @@
 /**@jsx jsx */
 
 import React from "react"
+import {useState} from "react";
 import { Link } from "gatsby"
 import { css } from '@emotion/react'
 import { jsx } from '@emotion/react'
 
 import * as styles from '../styles/_header.module.scss'
 
-export default () => (
-  <header className={styles.header}>
-    <h1 className={styles.logo}>Laugh Life</h1>
-    <div className={styles.navWrap}>
-        <div className={styles.nav}>
-        <Link to={"/company/"}>Company</Link>
-        <Link to={"/Business/"}>Business</Link>
-        <Link to={"/Products/"}>Products</Link>
-        <Link to={"/News/"}>News</Link>
-        <Link to={"/contact/"} className={styles.contact}>Contact</Link>
-        </div>
-        <div className={styles.humbtn}>
 
-        </div>
-    </div>
-  </header>
-)
+// const [openMenu, setOpenMenu] = useState(false);
+// const menuFunction = () => {
+//   setOpenMenu(!openMenu);
+// }
+// export default () => (
+//   <header className={styles.header}>
+//     <h1 className={styles.logo}>Laugh Life</h1>
+//     <div className={styles.navWrap}>
+//         <div className={styles.nav}>
+//         <Link to={"/company/"}>Company</Link>
+//         <Link to={"/Business/"}>Business</Link>
+//         <Link to={"/Products/"}>Products</Link>
+//         <Link to={"/News/"}>News</Link>
+//         <Link to={"/contact/"} className={styles.contact}>Contact</Link>
+//         </div>
+//         <div className={styles.openMenu} onClick={menuFunction}>
+//             <span className={openMenu ? styles.open : undefined}></span>
+//             <span className={openMenu ? styles.open : undefined}></span>
+//         </div>
+//     </div>
+//   </header>
+// )
 
+const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const menuFunction = () => {
+      setOpenMenu(!openMenu);
+    }
+  
+    return(
+        <header className={styles.header}>
+        <h1 className={styles.logo}>Laugh Life</h1>
+        <div className={styles.navWrap}>
+            <div className={styles.nav}>
+            <Link to={"/company/"}>Company</Link>
+            <Link to={"/Business/"}>Business</Link>
+            <Link to={"/Products/"}>Products</Link>
+            <Link to={"/News/"}>News</Link>
+            <Link to={"/contact/"} className={styles.contact}>Contact</Link>
+            </div>
+            <div className={styles.openMenu} onClick={menuFunction}>
+                <span className={openMenu ? styles.open : styles.close}></span>
+                <span className={openMenu ? styles.open : styles.close}></span>
+            </div>
+        </div>
+      </header>
+    );
+  };
+  
+  export default Header;
